a410670d6ee6a2fd85ec03a3eebe6cc31ea32dcf
Nam Tran <nam.tran@enlabsoftware.com>
2025-02-12 10:16:23 +0700

    feat: add LoginForm and SignupForm components for user authentication

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { useActionState, useState } from "react";
+
+import { handleLogin } from "@/actions/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { LoginResponse } from "@/shared/types/auth/login.type";
+import { useTranslation } from "@/providers/translation.provider";
+import Link from "next/link";
+import { ROUTES } from "@/shared/constants";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { AlertCircle, Loader2 } from "lucide-react";
+import { Alert, AlertTitle, AlertDescription } from "../ui/alert";
+
+const initialState: LoginResponse = {
+  success: false,
+  message: "",
+  errors: {},
+};
+
+const formSchema = z.object({
+  email: z.string().nonempty({ message: "email_required" }).email(),
+  password: z.string().nonempty({ message: "password_required" }),
+});
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const [LoginResponse, setLoginResponse] = useState(initialState);
+
+  const { dictionary } = useTranslation();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    const response = await handleLogin(data);
+    setLoginResponse(response);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      {LoginResponse.message && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{LoginResponse.message}</AlertDescription>
+        </Alert>
+      )}
+
+      <Card className="rounded-2xl">
+        <CardHeader>
+          <CardTitle className="text-2xl">{dictionary.log_in.title}</CardTitle>
+          <CardDescription>{dictionary.log_in.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="on">
+              <div className="flex flex-col gap-1">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{dictionary.form.email}</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="flex items-center">
+                          <FormLabel>{dictionary.form.password}</FormLabel>
+                          <Link
+                            href="#"
+                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </Link>
+                        </div>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <Button
+                  disabled={form.formState.isSubmitting}
+                  type="submit"
+                  className="w-full mb-2"
+                >
+                  <Loader2
+                    className={cn("animate-spin", {
+                      hidden: !form.formState.isSubmitting,
+                    })}
+                  />
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                {dictionary.log_in.have_account}{" "}
+                <Link
+                  href={ROUTES.SIGNUP}
+                  className="underline underline-offset-4"
+                >
+                  {dictionary.sign_up.title}
+                </Link>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ components/auth/signup-form.tsx
@@ -0,0 +1,172 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { handleSignUp } from "@/actions/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useTranslation } from "@/providers/translation.provider";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import Link from "next/link";
+import { ROUTES } from "@/shared/constants";
+
+const formSchema = z
+  .object({
+    email: z
+      .string()
+      .nonempty({ message: "email_required" })
+      .email({ message: "email_invalid" }),
+    username: z.string().nonempty({ message: "username_required" }),
+    password: z
+      .string()
+      .nonempty({ message: "password_required" })
+      .min(8, { message: "password_min_length" }),
+    confirmPassword: z
+      .string()
+      .nonempty({ message: "confirm_password_required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "confirm_password_not_match",
+    path: ["confirmPassword"],
+  });
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const { dictionary } = useTranslation();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    const result = await handleSignUp(data);
+    console.log(result);
+    //TODO: Show alert when there is an error
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="rounded-2xl">
+        <CardHeader>
+          <CardTitle className="text-2xl">{dictionary.sign_up.title}</CardTitle>
+          <CardDescription>{dictionary.sign_up.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="on">
+              <div className="flex flex-col">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{dictionary.form.email}</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{dictionary.form.username}</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{dictionary.form.password}</FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          {dictionary.form.confirm_password}
+                        </FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <Button type="submit" className="w-full">
+                  Sign up
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Sign up with Google
+                </Button>
+              </div>
+
+              <div className="mt-4 text-center text-sm">
+                {dictionary.sign_up.have_account}{" "}
+                <Link
+                  href={ROUTES.LOGIN}
+                  className="underline underline-offset-4"
+                >
+                  {dictionary.log_in.title}
+                </Link>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
