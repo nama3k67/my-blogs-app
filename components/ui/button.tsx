cba267ebd7d4d29d8815d6b31080445d97033e02
Nam Tran <nama3k67lnq@gmail.com>
2025-03-05 09:39:17 +0700

    feat: enhance UI components with improved styles and new separator and tabs components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/shared/libs/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 bg-background hover:bg-accent hover:text-accent-foreground",
+          "shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 bg-background hover:bg-accent hover:text-accent-foreground dark:ring-white/10 dark:hover:ring-white/20",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
