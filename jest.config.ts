728d585e086de4ad5fcab7e2df49825702b7c534
Nam Tran <nama3k67lnq@gmail.com>
2025-03-28 09:01:21 +0700

    feat: add Jest configuration and setup for testing with Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customConfig: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+};
+
+export default createJestConfig(customConfig);
